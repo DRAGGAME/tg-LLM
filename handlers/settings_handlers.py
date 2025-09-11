f7f9f00179618cb6b53f1bb023b612c8e962da4c
DRAGGAME <maxutnov@mail.ru>
2025-09-11 19:54:20 +0300

    14:33 edit line

--- handlers/settings_handlers.py
+++ handlers/settings_handlers.py
@@ -11,26 +11,27 @@ fabric_ml = InlineChoiceFabric()
 sqlbase_request = UserQueries()
 
 
-@settings_router.callback_query(InlineChoiceTextSettings.filter(F.mode_for_text=="size"))
+@settings_router.callback_query(InlineChoiceTextSettings.filter(F.mode_for_text == "size"))
 async def edit_size_text(callback: CallbackQuery):
     await sqlbase_request.connect()
     last_mode = await sqlbase_request.get_user_model(str(callback.from_user.id))
     await sqlbase_request.close()
-    keyboard = await fabric_ml.choice_settings_text(last_mode[0][0])
+    keyboard = await fabric_ml.choice_size_text(1)
 
     await callback.message.edit_reply_markup(reply_markup=keyboard)
     await callback.answer()
 
 
-@settings_router.callback_query(InlineChoiceTextSettings.filter(F.mode_for_text=="Questions"))
+@settings_router.callback_query(InlineChoiceTextSettings.filter(F.mode_for_text == "Questions"))
 async def edit_question_level(callback: CallbackQuery):
     await sqlbase_request.connect()
     last_mode = await sqlbase_request.get_user_model(str(callback.from_user.id))
     await sqlbase_request.close()
-    keyboard = await fabric_ml.choice_question_level(last_mode[0][0])
+    keyboard = await fabric_ml.choice_question_level(1)
 
     await callback.message.edit_reply_markup(reply_markup=keyboard)
     await callback.answer()
+
 
 @settings_router.callback_query(InlineChoiceSize.filter(F.size_text.in_([1, 2, 3])))
 async def change_size_text(callback: CallbackQuery, callback_data: CallbackData):
@@ -46,6 +47,7 @@ async def change_size_text(callback: CallbackQuery, callback_data: CallbackData)
         keyboard = await fabric_ml.choice_size_text(3)
         await callback.message.edit_reply_markup(reply_markup=keyboard)
         await callback.answer("Размер текста изменен на 3")
+
 
 @settings_router.callback_query(QuestionLevelChoice.filter(F.question_level.in_([1, 2, 3])))
 async def change_level_detalisation(callback: CallbackQuery, callback_data: CallbackData):
@@ -63,4 +65,3 @@ async def change_level_detalisation(callback: CallbackQuery, callback_data: Call
         keyboard = await fabric_ml.choice_question_level(3)
         await callback.message.edit_reply_markup(reply_markup=keyboard)
         await callback.answer("Уровень детализации изменен на 3")
-
