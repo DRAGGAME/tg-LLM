98f0f4f6da797f9ab9ae54e7390949ce0edeb4ca
DRAGGAME <maxutnov@mail.ru>
2025-09-20 00:02:28 +0300

    FINAL

--- handlers/begin_handler.py
+++ handlers/begin_handler.py
@@ -55,4 +55,4 @@ async def cancel_handler(callback: CallbackQuery, state: FSMContext):
 
 @begin_router.message(Command(commands=['Help', 'help']))
 async def help_handler(message: Message):
-    await message.reply("Отправте файл боту и он сделает его краткое описание, и напишет вопросы к нему!\n\nВНИМАНИЕ!\nБОТ РАБОТАЕТ ТОЛЬКО С ФАЙЛАМИ, У КОТОРЫХ ИМЕЕТСЯ ТЕКСТ ВНУТРИ.\nМожно отправить файл в виде таблицы")
+    await message.reply("Отправте файл боту и он сделает его краткое описание, и напишет вопросы к нему!\n\nВНИМАНИЕ!\nБОТ РАБОТАЕТ ТОЛЬКО С ФАЙЛАМИ, У КОТОРЫХ ИМЕЕТСЯ ТЕКСТ ВНУТРИ.")
